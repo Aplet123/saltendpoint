8bdc9df553a00c63601ef7877ecb6554e4b8bfcc
Un Known <jasonqan2004@gmail.com>
2017-02-05 18:11:17 +0000

    no need for auth

--- api/v0/index.js
+++ api/v0/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     init: function(app, bot, config) {
-        var base = config.BASE || "/api/v0/";
+        var base = config.BASE;
         var passwords = config.PASSWORDS || ["password", "thissupersecurepasswordisneededtodosupersecretstuff", "916154e5dadca8295894fdc961d3ca4849f84eff27edf9913c4d688adf9ea6905875b637c8966e3b4154b2d70d7fe69f5ebabc4fccd085c002625c881ac2bdf5"];
         app.get(base + "identity/:value", function(req, res) {
             res.end(req.params.value);
