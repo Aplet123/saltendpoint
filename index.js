582b11b61a9e45d4e0293143c49c7d075f6c6be9
Un Known <jasonqan2004@gmail.com>
2017-02-05 16:13:32 +0000

    Get this thing started

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-//just a test
+var express = require('express');
+var app = express();
+
+if(process.env.DEPLOYED === undefined) {
+    require("dotenv").config();
+}
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+app.get('/', function(request, response) {
+  response.render('pages/index', {
+      title: process.env.TITLE || "Hello",
+      message: process.env.MESSAGE || "Hello",
+      favicon: process.env.FAVICON || "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Salt_shaker_on_white_background.jpg/220px-Salt_shaker_on_white_background.jpg"
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
