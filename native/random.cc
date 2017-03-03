9a8641d3ae25ecf0adaee89bf1d99334c49f29ad
Un Known <jasonqan2004@gmail.com>
2017-03-03 23:35:38 +0000

    Extreme testing stuff

--- native/random.cc
+++ native/random.cc
@@ -1,5 +1,7 @@
 #include <node.h>
 #include <random>
+#include <iostream>
+
 
 namespace demo {
 
@@ -9,14 +11,37 @@ using v8::Local;
 using v8::Object;
 using v8::String;
 using v8::Value;
+using v8::Boolean;
+using v8::Exception;
+using std::cout;
+using std::cin;
+using std::endl;
 
-void Method(const FunctionCallbackInfo<Value>& args) {
+void ThrowException(Isolate* isolate, const char * exception) {
+    isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, exception)));
+}
+
+void Bernoulli(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
-  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
+  int argLength = args.Length();
+  if (argLength == 0) {
+      ThrowException(isolate, "Too few arguments");
+      return;
+  }
+  std::default_random_engine generator;
+  if (! args[0] -> IsNumber()) {
+      ThrowException(isolate, "Incorrect typing");
+      return;
+  }
+  double doub = args[0] -> NumberValue();
+  std::bernoulli_distribution dist(doub);
+  cout << doub << "\n" << dist(generator) << "\n";
+  Local <Boolean> ret = Boolean::New(isolate, dist(generator));
+  args.GetReturnValue().Set(ret);
 }
 
 void init(Local<Object> exports) {
-  NODE_SET_METHOD(exports, "hello", Method);
+  NODE_SET_METHOD(exports, "bernoulli_distribution", Bernoulli);
 }
 
 NODE_MODULE(random, init)
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,1 @@
-npm list -g node-gyp
-exists=$?
-if ! [ exists = 0]; then
-    npm install -g node-gyp
-fi
 node-gyp rebuild
